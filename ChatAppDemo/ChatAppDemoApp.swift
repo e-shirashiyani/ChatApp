04923ecb2bc70fc1bd3300a7d70fbf9d2b6572aa
e.shirashiyani <e.shirashiyani>
2022-02-08 18:52:41 +0330

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by e.shirashiyani on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
